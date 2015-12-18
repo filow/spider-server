e3986f1b0e551e1ee99aaaf968ef279de2dabddd
filow <lirenhai427@gmail.com>
2015-12-18 10:10:59 +0800

    Add d.ts to project files

---
+++ typings/tsd.d.ts
@@ -0,0 +1,15 @@
+
+/// <reference path="express/express.d.ts" />
+/// <reference path="mime/mime.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="body-parser/body-parser.d.ts" />
+/// <reference path="async/async.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="cli-color/cli-color.d.ts" />
+/// <reference path="redis/redis.d.ts" />
+/// <reference path="cheerio/cheerio.d.ts" />
+/// <reference path="form-data/form-data.d.ts" />
+/// <reference path="request/request.d.ts" />
+/// <reference path="superagent/superagent.d.ts" />
+/// <reference path="mongodb/mongodb.d.ts" />
